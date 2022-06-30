4025cb84d250b310f4d7e6ee8deb5ed2b219a519
ericrothmuller <eric@skymarketingco.com>
2022-06-29 23:05:37 -0700

    Added pseudo code for getting the rows to change color

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,38 @@
 var today = moment();
 
 $("#currentDay").text(today.format("dddd, MMMM do"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Psuedo code for how to get the rows to change color
+
+// Get the current hour in a single number with Math.floor
+
+// If that hour matches a row number (based on hour), apply the .present class
+
+// If a row is greater than that number, apply the .future class
+
+// is a row is less than that number, apply the .past class
