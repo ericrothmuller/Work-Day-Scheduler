83801aee41a2e1254dd6ac6d1ab69816076106b2
ericrothmuller <eric@skymarketingco.com>
2022-06-29 23:47:30 -0700

    Added a series of if, else if, else statements to change the rows colors based on the current time. It'll display the row as red if it is the current hour, grey if in the past, and green if it's in the future.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,9 @@
 var today = moment();
 
+var hour = moment().hour();
+
+console.log(hour);
+
 $("#currentDay").text(today.format("dddd, MMMM do"));
 
 
@@ -7,6 +11,104 @@ $("#currentDay").text(today.format("dddd, MMMM do"));
 
 
 
+if (hour < 9) {
+    const changeColor = document.getElementById("9hour");
+    changeColor.classList.add("future");
+    } else if (hour == 9 ) {
+    const changeColor = document.getElementById("9hour");
+    changeColor.classList.add("present");
+    } else {
+    const changeColor = document.getElementById("9hour");
+    changeColor.classList.add("past");
+    }
+
+if (hour < 10) {
+    const changeColor = document.getElementById("10hour");
+    changeColor.classList.add("future");
+    } else if (hour == 10 ) {
+    const changeColor = document.getElementById("10hour");
+    changeColor.classList.add("present");
+    } else {
+    const changeColor = document.getElementById("10hour");
+    changeColor.classList.add("past");
+    }
+
+if (hour < 11) {
+    const changeColor = document.getElementById("11hour");
+    changeColor.classList.add("future");
+    } else if (hour == 11 ) {
+    const changeColor = document.getElementById("11hour");
+    changeColor.classList.add("present");
+    } else {
+    const changeColor = document.getElementById("11hour");
+    changeColor.classList.add("past");
+    }
+
+if (hour < 12) {
+    const changeColor = document.getElementById("12hour");
+    changeColor.classList.add("future");
+    } else if (hour == 12 ) {
+    const changeColor = document.getElementById("12hour");
+    changeColor.classList.add("present");
+    } else {
+    const changeColor = document.getElementById("12hour");
+    changeColor.classList.add("past");
+    }
+
+if (hour < 13) {
+    const changeColor = document.getElementById("13hour");
+    changeColor.classList.add("future");
+    } else if (hour == 13 ) {
+    const changeColor = document.getElementById("13hour");
+    changeColor.classList.add("present");
+    } else {
+    const changeColor = document.getElementById("13hour");
+    changeColor.classList.add("past");
+    }
+
+if (hour < 14) {
+    const changeColor = document.getElementById("14hour");
+    changeColor.classList.add("future");
+    } else if (hour == 14 ) {
+    const changeColor = document.getElementById("14hour");
+    changeColor.classList.add("present");
+    } else {
+    const changeColor = document.getElementById("14hour");
+    changeColor.classList.add("past");
+    }
+
+if (hour < 15) {
+    const changeColor = document.getElementById("15hour");
+    changeColor.classList.add("future");
+    } else if (hour == 15 ) {
+    const changeColor = document.getElementById("15hour");
+    changeColor.classList.add("present");
+    } else {
+    const changeColor = document.getElementById("15hour");
+    changeColor.classList.add("past");
+    }
+
+if (hour < 16) {
+    const changeColor = document.getElementById("16hour");
+    changeColor.classList.add("future");
+    } else if (hour == 16 ) {
+    const changeColor = document.getElementById("16hour");
+    changeColor.classList.add("present");
+    } else {
+    const changeColor = document.getElementById("16hour");
+    changeColor.classList.add("past");
+    }
+
+if (hour < 17) {
+    const changeColor = document.getElementById("17hour");
+    changeColor.classList.add("future");
+    } else if (hour == 17 ) {
+    const changeColor = document.getElementById("17hour");
+    changeColor.classList.add("present");
+    } else {
+    const changeColor = document.getElementById("17hour");
+    changeColor.classList.add("past");
+    }
 
 
 
@@ -15,24 +117,12 @@ $("#currentDay").text(today.format("dddd, MMMM do"));
 
 
 
+// Pseudo code for how to get the rows to change color
 
+// Get the current hour in a single number - done
 
+// If that hour matches a row number (based on hour), apply the .present class - done
 
+// If a row is greater than that number, apply the .future class - done
 
-
-
-
-
-
-
-
-
-// Psuedo code for how to get the rows to change color
-
-// Get the current hour in a single number with Math.floor
-
-// If that hour matches a row number (based on hour), apply the .present class
-
-// If a row is greater than that number, apply the .future class
-
-// is a row is less than that number, apply the .past class
+// is a row is less than that number, apply the .past class - done
