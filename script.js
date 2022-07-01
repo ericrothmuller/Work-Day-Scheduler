f7b26b3d2d4c469b136232388da9e2a48fa8165c
ericrothmuller <eric@skymarketingco.com>
2022-06-30 23:25:11 -0700

    Working on localStorage. Committing so BCS helper can take a look at what I'm working on.

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ var hour = moment().hour();
 
 console.log(hour);
 
-$("#currentDay").text(today.format("dddd, MMMM Do"));
-
-
+$("#currentDay").text(today.format("LLLL"));
 
 
 
@@ -111,4 +109,50 @@ if (hour < 17) {
     }
 
 
+$(".saveBtn").on("click", function (event) {
+    var saveItem = event.target;
+    var savedLocal = $(this).siblings("textarea").val();
+    if (savedLocal == "") {
+        console.log("empty");
+    } else {
+        console.log("text");
+        localStorage.setItem(saveItem.dataset.save, savedLocal);
 
+        console.log($(this).siblings("textarea"));
+    }
+});
+
+// var ninehourbtn = getElementById("ninehourbtn");
+// var tenhourbtn = getElementById("tenhourbtn");
+// var elevenhourbtn = getElementById("elevenhourbtn");
+// var twelvehourbtn = getElementById("twelvehourbtn");
+// var thirteenhourbtn = getElementById("thirteenhourbtn");
+// var fourteenhourbtn = getElementById("fourteenhourbtn");
+// var fifteenhourbtn = getElementById("fifteenhourbtn");
+// var sixteenhourbtn = getElementById("sixteenhourbtn");
+// var seventeenhourbtn = getElementById("seventeenhourbtn");
+
+
+var nineText = document.getElementById("9hourtext");
+var tenText = document.getElementById("10hourtext");
+var elevenText = document.getElementById("11hourtext");
+var twelveText = document.getElementById("12hourtext");
+var thirteenText = document.getElementById("13hourtext");
+var fourteenText = document.getElementById("14hourtext");
+var fifteenText = document.getElementById("15hourtext");
+var sixteenText = document.getElementById("16hourtext");
+var seventeenText = document.getElementById("17hourtext");
+
+function saveText() {
+    nineText.text(localStorage.getItem("ninehourbtn"));
+    tenText.text(localStorage.getItem("tenhourbtn"));
+    elevenText.text(localStorage.getItem("elevenhourbtn"));
+    twelveText.text(localStorage.getItem("twelvehourbtn"));
+    thirteenText.text(localStorage.getItem("thirteenhourbtn"));
+    fourteenText.text(localStorage.getItem("fourteenhourbtn"));
+    fifteenText.text(localStorage.getItem("fifteenhourbtn"));
+    sixteenText.text(localStorage.getItem("sixteenhourbtn"));
+    seventeenText.text(localStorage.getItem("seventeenhourbtn"));
+}
+
+saveText();
